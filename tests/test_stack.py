d1092d68f4a646f570e303823453504dcf428319
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-14 19:56:04 +0300

    add tests for class Node

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,11 @@
-"""Здесь надо написать тесты с использованием unittest для модуля stack."""
+import unittest
+from src import stack
+
+test_node_1 = stack.Node('test_1', 1)
+test_node_2 = stack.Node('test_2', 2)
+
+
+class TestNode(unittest.TestCase):
+    def test_node(self):
+        self.assertEqual(test_node_1.data, 'test_1')
+        self.assertEqual(test_node_2.next_node, 2)
