fd7e6bc71f08da579f36dfc5fcb4f6dac1bee200
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-14 22:38:13 +0300

    add test for enqueue method of class Queue

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -1,1 +1,16 @@
-"""Здесь надо написать тесты с использованием unittest для модуля queue."""
+import unittest
+from src import queue
+
+test_queue = queue.Queue()
+test_queue.enqueue('test_1')
+test_queue.enqueue('test_2')
+test_queue.enqueue('test_3')
+
+
+class TestQueue(unittest.TestCase):
+    def test_queue_enqueue(self):
+        self.assertEqual(test_queue.head.data, 'test_1')
+        self.assertEqual(test_queue.head.next_node.data, 'test_2')
+        self.assertEqual(test_queue.head.next_node.next_node.data, 'test_3')
+        self.assertEqual(test_queue.tail.data, 'test_3')
+        self.assertEqual(test_queue.tail.next_node, None)
