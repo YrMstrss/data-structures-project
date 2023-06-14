449f0a300c1ca47cd757d957c1673a234348bfd3
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-14 20:38:22 +0300

    add test for class Stack

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -9,3 +9,16 @@ class TestNode(unittest.TestCase):
     def test_node(self):
         self.assertEqual(test_node_1.data, 'test_1')
         self.assertEqual(test_node_2.next_node, 2)
+
+
+test_stack = stack.Stack()
+test_stack.push('data1')
+test_stack.push('data2')
+test_stack.push('data3')
+
+
+class TestStack(unittest.TestCase):
+    def test_stack(self):
+        self.assertEqual(test_stack.top.data, 'data3')
+        self.assertEqual(test_stack.top.next_node.data, 'data2')
+        self.assertEqual(test_stack.top.next_node.next_node.data, 'data1')
