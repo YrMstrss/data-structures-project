44ac897bfc98832a0a5fd55114f4cb1fb58e4513
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-15 21:37:57 +0300

    update test_queue_enqueue and add test_queue_str

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -5,6 +5,7 @@ test_queue = queue.Queue()
 test_queue.enqueue('test_1')
 test_queue.enqueue('test_2')
 test_queue.enqueue('test_3')
+test_queue.enqueue('test_4')
 
 
 class TestQueue(unittest.TestCase):
@@ -12,5 +13,13 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(test_queue.head.data, 'test_1')
         self.assertEqual(test_queue.head.next_node.data, 'test_2')
         self.assertEqual(test_queue.head.next_node.next_node.data, 'test_3')
-        self.assertEqual(test_queue.tail.data, 'test_3')
+        self.assertEqual(test_queue.head.next_node.next_node.next_node.data, 'test_4')
+        self.assertEqual(test_queue.tail.data, 'test_4')
         self.assertEqual(test_queue.tail.next_node, None)
+
+    def test_queue_str(self):
+        new_test_queue = queue.Queue()
+        self.assertEqual(str(new_test_queue), "")
+        new_test_queue.enqueue('test_1')
+        new_test_queue.enqueue('test_2')
+        self.assertEqual(str(new_test_queue), "test_1\ntest_2")
