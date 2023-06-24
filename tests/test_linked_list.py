df96a182077ae1b0f31aff51c93c081e2443b8fc
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-24 20:49:12 +0300

    add test for Node

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием unittest для модуля linked_list."""
+import unittest
+from src.linked_list import Node, LinkedList
+
+n = Node(1, 2)
+test_linked_list = LinkedList()
+
+
+class TestNode(unittest.TestCase):
+    def test_node(self):
+        self.assertEqual(n.data, 1)
+        self.assertEqual(n.next_node, 2)
+
+
+class TestLinkedList(unittest.TestCase):
+    pass
