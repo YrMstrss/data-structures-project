48324fbd758b6a57e0da775c0154e1539cad3336
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-24 21:37:33 +0300

    realize LinkedList

--- src/linked_list.py
+++ src/linked_list.py
@@ -7,14 +7,32 @@ class Node:
 
 class LinkedList:
     """Класс для односвязного списка"""
+    def __init__(self):
+        self.head = None
+        self.tail = None
 
     def insert_beginning(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в начало связанного списка"""
-        pass
+        if self.head == self.tail is None:
+            self.head = Node(data, None)
+            self.tail = Node(data, None)
+        else:
+            self.head = Node(data, self.head)
 
     def insert_at_end(self, data: dict) -> None:
         """Принимает данные (словарь) и добавляет узел с этими данными в конец связанного списка"""
-        pass
+
+        new_node = Node(data, None)
+
+        if self.head == self.tail is None:
+            self.head = new_node
+            self.tail = new_node
+        elif self.head.next_node is None:
+            self.head.next_node = new_node
+            self.tail = new_node
+        else:
+            self.tail.next_node = new_node
+            self.tail = new_node
 
     def __str__(self) -> str:
         """Вывод данных односвязного списка в строковом представлении"""
@@ -24,8 +42,8 @@ class LinkedList:
 
         ll_string = ''
         while node:
-            ll_string += f' {str(node.data)} ->'
+            ll_string += f'{str(node.data)} -> '
             node = node.next_node
 
-        ll_string += ' None'
+        ll_string += 'None'
         return ll_string
