20244dd703a5c420db8821f887d9a1d18f6e0942
Valerya Yakushina <v.yaku.bmstu@gmail.com>
2023-06-24 20:50:08 +0300

    add test for LinkedList insert_beginning

--- tests/test_linked_list.py
+++ tests/test_linked_list.py
@@ -12,4 +12,12 @@ class TestNode(unittest.TestCase):
 
 
 class TestLinkedList(unittest.TestCase):
-    pass
+
+    def test_insert_beginning(self):
+        test_linked_list.insert_beginning({'a': 1})
+        self.assertEqual(test_linked_list.head.data, {'a': 1})
+        test_linked_list.insert_beginning({'a': 2})
+        self.assertEqual(test_linked_list.head.data, {'a': 2})
+        self.assertEqual(test_linked_list.head.next_node.data, {'a': 1})
+        self.assertEqual(test_linked_list.tail.data, {'a': 1})
+
